d47c02c66276adc6e9440fdb5e998e45d9e3214d
Anders Windelhed <anders.windelhed@tv4.se>
2025-03-25 22:49:56 +0100

    Getting position and commands from request body

--- routes/enter_path.ts
+++ routes/enter_path.ts
@@ -1,7 +1,16 @@
 import { type Request, type Response } from 'express'
+import Position from '../types/position'
+import Command from '../types/command'
 
-const enterPath = async (_: Request, response: Response) => {
-  response.send({ json: true })
+type Body = {
+  start: Position
+  commands: Command[]
+}
+
+const enterPath = async (request: Request, response: Response) => {
+  const { start, commands } = request.body as Body
+
+  response.send({ start, commands })
 }
 
 export default enterPath
