e6095768f249c1fd16bf9a808754e3d5f0eda070
Anders Windelhed <anders.windelhed@tv4.se>
2025-03-26 21:31:15 +0100

    Awating database connection before listening on port

--- index.ts
+++ index.ts
@@ -1,12 +1,26 @@
 import express from 'express'
 import config from './config'
 import enterPath from './routes/enter_path'
+import sequelize from './db'
 
-export const app = express()
+const app = express()
 
-app.listen(config.port, () => {
-  console.log(`Running on port ${config.port} 🥳`)
-})
 app.use(express.json())
 
 app.post('/tibber-developer-test/enter-path', enterPath)
+
+const run = async () => {
+  try {
+    await sequelize.authenticate()
+    console.log('Connected to database')
+  } catch (e: unknown) {
+    console.error('Database setup failed:', e)
+    process.exit(1)
+  }
+
+  app.listen(config.port, () => {
+    console.log(`Running on port ${config.port} 🥳`)
+  })
+}
+
+run()
