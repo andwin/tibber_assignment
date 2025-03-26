b4db6ba8f964cfa00c2f2e364ae0a93ae0294e47
Anders Windelhed <anders.windelhed@tv4.se>
2025-03-26 21:33:43 +0100

    Saving execution to database

--- routes/enter_path.ts
+++ routes/enter_path.ts
@@ -1,7 +1,9 @@
 import { type Request, type Response } from 'express'
-import Position from '../types/position'
-import Command from '../types/command'
+import { CreationAttributes } from '@sequelize/core'
+import type Position from '../types/position'
+import type Command from '../types/command'
 import processCommands from '../lib/process_commands'
+import { Execution } from '../db'
 
 type Body = {
   start: Position
@@ -10,10 +12,23 @@ type Body = {
 
 const enterPath = async (request: Request, response: Response) => {
   const { start, commands } = request.body as Body
-
   const result = processCommands(start, commands)
 
-  response.send({ start, commands, result })
+  try {
+    const data: CreationAttributes<Execution> = {
+      timestamp: new Date(),
+      commands: commands.length,
+      result,
+      duration: 0,
+    }
+    const execution = await Execution.create(data)
+    console.log('Execution created:', data)
+
+    response.send({ execution })
+  } catch (e: unknown) {
+    console.error('Failed to create execution:', e)
+    response.status(500).send({ error: 'Failed to create execution' })
+  }
 }
 
 export default enterPath
